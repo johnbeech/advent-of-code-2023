47d63658eb72578fe13b632ce938b3f21e0f5434
John Beech <johnbeech.github@mkv25.net>
2023-12-10 15:21:41 +0000

    Solve directionality following path from start

--- solutions/day10/solution.js
+++ solutions/day10/solution.js
@@ -4,29 +4,36 @@ const fromHere = position(__dirname)
 const report = (...messages) => console.log(`[${require(fromHere('../../package.json')).logName} / ${__dirname.split(path.sep).pop()}]`, ...messages)
 
 async function run () {
-  const input = (await read(fromHere('input.txt'), 'utf8')).trim()
+  const input = (await read(fromHere('example.txt'), 'utf8')).trim()
 
   await solveForFirstStar(input)
   await solveForSecondStar(input)
 }
 
 const directions = {
-  UP: { name: 'up', x: 0, y: -1 },
-  DOWN: { name: 'down', x: 0, y: 1 },
-  LEFT: { name: 'left', x: -1, y: 0 },
-  RIGHT: { name: 'right', x: 1, y: 0 }
+  UP: { name: 'up', x: 0, y: -1, symbol: '↑', startRotation: 0 },
+  DOWN: { name: 'down', x: 0, y: 1, symbol: '↓', startRotation: 180 },
+  LEFT: { name: 'left', x: -1, y: 0, symbol: '←', startRotation: 270 },
+  RIGHT: { name: 'right', x: 1, y: 0, symbol: '→', startRotation: 90 }
+}
+
+const rotations = {
+  0: { direction: directions.UP, symbol: '↑' },
+  90: { direction: directions.RIGHT, symbol: '→' },
+  180: { direction: directions.DOWN, symbol: '↓' },
+  270: { direction: directions.LEFT, symbol: '←' }
 }
 
 const pipeTypes = {
-  '-': { exits: [directions.LEFT, directions.RIGHT], name: 'horizontal' },
-  '|': { exits: [directions.UP, directions.DOWN], name: 'vertical' },
-  F: { exits: [directions.RIGHT, directions.DOWN], name: 'F corner' },
-  7: { exits: [directions.LEFT, directions.DOWN], name: '7 corner' },
-  L: { exits: [directions.UP, directions.RIGHT], name: 'L corner' },
-  J: { exits: [directions.UP, directions.LEFT], name: 'J corner' },
-  '.': { exits: [], name: 'floor' },
-  S: { exits: [...Object.values(directions)], name: 'start' },
-  '?': { exits: [], name: 'unknown' }
+  '-': { exits: [directions.LEFT, directions.RIGHT], name: 'horizontal', symbol: '-', rotation: 0 },
+  '|': { exits: [directions.UP, directions.DOWN], name: 'vertical', symbol: '|', rotation: 0 },
+  F: { exits: [directions.DOWN, directions.RIGHT], name: 'F corner', symbol: '┌', rotation: 90 },
+  7: { exits: [directions.DOWN, directions.LEFT], name: '7 corner', symbol: '┐', rotation: -90 },
+  L: { exits: [directions.UP, directions.RIGHT], name: 'L corner', symbol: '└', rotation: -90 },
+  J: { exits: [directions.UP, directions.LEFT], name: 'J corner', symbol: '┘', rotation: 90 },
+  '.': { exits: [], name: 'floor', symbol: '.', rotation: 0 },
+  S: { exits: [...Object.values(directions)], name: 'start', symbol: 'S', rotation: 0 },
+  '?': { exits: [], name: 'unknown', symbol: '?' }
 }
 
 function parsePipeMap (input) {
@@ -34,11 +41,12 @@ function parsePipeMap (input) {
     locations: {},
     pipes: []
   }
-  pipeMap.pipes = input.split('\n').map(line => line.trim()).filter(line => line.length > 0).forEach((line, y) => {
+  pipeMap.pipes = input.split('\n').map(line => line.trim()).filter(line => line.length > 0).map((line, y) => {
     const row = line.split('').map((char, x) => {
       const pipe = pipeTypes[char] ?? pipeTypes['?']
-      pipeMap.locations[`${x},${y}`] = { x, y, ...pipe }
-      return pipe
+      const location = { x, y, ...pipe }
+      pipeMap.locations[`${x},${y}`] = location
+      return location
     })
     return row
   })
@@ -48,6 +56,7 @@ function parsePipeMap (input) {
 function followRoute (pipeMap, startDirection) {
   const route = []
   const startLocation = Object.values(pipeMap.locations).find(location => location.name === 'start')
+  let rotation = startDirection.startRotation
   let currentLocation = startLocation
   let currentDirection = startDirection
   while (route.includes(currentLocation) === false) {
@@ -67,6 +76,14 @@ function followRoute (pipeMap, startDirection) {
     if (!newDirection) {
       console.log('Couldn\'t find an exit', { currentLocation, nextLocation, currentDirection })
       break
+    }
+
+    nextLocation.exit = newDirection
+    const pipeRotation = (newDirection === nextLocation.exits[0]) ? nextLocation.rotation : -nextLocation.rotation
+    rotation = (720 + rotation - pipeRotation) % 360
+    console.log(rotations[rotation].symbol, pipeRotation)
+    if (nextLocation.symbol !== 'S') {
+      nextLocation.travel = rotations[rotation]?.symbol
     }
 
     currentDirection = newDirection
@@ -96,7 +113,67 @@ async function solveForFirstStar (input) {
   report('Solution 1:', solution)
 }
 
+function floodFill (locations, x, y, symbol) {
+  const visited = []
+  const start = locations[`${x},${y}`]
+  const startSymbol = start.symbol
+  const unvisited = [start]
+  const exits = Object.values(directions)
+  console.log('Flood filling', { x, y, symbol, startSymbol })
+  while (unvisited.length > 0) {
+    const location = unvisited.pop()
+    if (location === undefined) {
+      // ignore
+    } else if (location.symbol === symbol) {
+      // ignore
+    } else if (visited.includes(location)) {
+      // ignore
+    } else if (location.symbol !== startSymbol) {
+      // ignore
+    } else {
+      location.symbol = symbol
+      visited.push(location)
+      const newLocations = exits.map(exit => locations[`${location.x + exit.x},${location.y + exit.y}`])
+        .filter(location => visited.includes(location) === false && unvisited.includes(location) === false)
+      unvisited.push(...newLocations)
+    }
+  }
+  console.log('Flood filled', visited.length, 'locations')
+}
+
 async function solveForSecondStar (input) {
+  const pipeMap = parsePipeMap(input)
+
+  const routes = Object.values(directions).map(direction => {
+    return followRoute(pipeMap, direction)
+  })
+
+  const fullRoute = routes.find(route => route.length > 2 && route.filter(location => location.name === 'start').length === 2)
+
+  console.log('Full route:', fullRoute)
+
+  Object.values(pipeMap.locations).forEach(location => {
+    if (!fullRoute.includes(location)) {
+      delete location.exits
+      location.symbol = '.'
+    }
+  })
+
+  const outside = 'o'
+  const inside = 'i'
+  floodFill(pipeMap.locations, 0, 0, outside)
+  const halfSize = Math.round(pipeMap.pipes.length / 2)
+  if (pipeMap.locations[`${halfSize},${halfSize}`].symbol !== outside) {
+    floodFill(pipeMap.locations, halfSize, halfSize, inside)
+  }
+
+  fullRoute.forEach(location => {
+    location.symbol = location.travel ?? location.symbol
+  })
+
+  const outputText = pipeMap.pipes.map(row => row.map(pipe => pipe.symbol).join('')).join('\n')
+  await write(fromHere('output.txt'), outputText, 'utf8')
+
   const solution = 'UNSOLVED'
   report('Solution 2:', solution)
 }
