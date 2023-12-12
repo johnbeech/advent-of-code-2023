870cf3c09523c7991cec3c13d9075c54c2374441
John Beech <johnbeech.github@mkv25.net>
2023-12-12 19:59:05 +0000

    Match patterns

--- solutions/day12/solution.js
+++ solutions/day12/solution.js
@@ -14,24 +14,66 @@ function parseSprings (input) {
   const lines = input.split('\n').map(line => line.trim()).filter(line => line.length)
   const springs = lines.map((line, index) => {
     const [conditionString, checkString] = line.split(' ')
-    const conditions = conditionString.split('')
+    const conditions = conditionString
+    const compressed = conditionString.replaceAll(/\.[.]+/g, '.')
     const checks = checkString.split(',').map(check => Number.parseInt(check, 10))
+    const blocks = compressed.split('.').filter(block => block.length).map(block => {
+      return {
+        str: block,
+        contiguous: block.split('').every(char => char === '#')
+      }
+    })
     return {
       index,
       conditions,
-      checks
+      compressed,
+      checks,
+      blocks
     }
   })
   return springs
 }
 
+function placeChecks (spring) {
+  const checks = [...spring.checks]
+  const blocks = [...spring.blocks]
+  let currentCheckBlock = []
+  while (blocks.length > 0) {
+    const currentBlock = blocks.shift()
+    while (checks.length > 0) {
+      const nextCheck = checks.shift()
+      currentCheckBlock.push(nextCheck)
+      const checkLength = currentCheckBlock.reduce((total, check) => total + check, currentCheckBlock.length - 1)
+      if (checkLength === currentBlock.str.length) {
+        // exact match
+        currentBlock.checks = [...currentCheckBlock]
+        break
+      } else if (checkLength > currentBlock.str.length) {
+        // step back
+        currentCheckBlock.pop()
+        checks.unshift(nextCheck)
+        break
+      } else {
+        // add more blocks
+      }
+    }
+    currentBlock.checks = [...currentCheckBlock]
+    currentCheckBlock = []
+  }
+  console.log('Checked blocks:', spring.blocks, spring.checks)
+  return spring
+}
+
 async function solveForFirstStar (input) {
   const springs = parseSprings(input)
+
+  springs.forEach(spring => {
+    placeChecks(spring)
+  })
 
   await write(fromHere('springs.json'), JSON.stringify(springs, null, 2), 'utf8')
 
   const solution = 'UNSOLVED'
-  report('Input:', input)
   report('Solution 1:', solution)
 }
 
