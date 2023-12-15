8811d9d944993c70951a8bb82e2c042ad81b0c4a
John Beech <johnbeech.github@mkv25.net>
2023-12-15 09:26:07 +0000

    Solve day 15 for second star

--- solutions/day15/solution.js
+++ solutions/day15/solution.js
@@ -32,7 +32,61 @@ async function solveForFirstStar (input) {
 }
 
 async function solveForSecondStar (input) {
-  const solution = 'UNSOLVED'
+  const instructions = input.split(',').map(instruction => {
+    const chars = instruction.split('')
+    const op1 = chars.pop()
+    const op2 = chars.pop()
+    const op = op1 === '-' ? '-' : '='
+    const focalLength = op === '=' ? Number.parseInt(op1) : 0
+    if (op === '-') {
+      chars.push(op2)
+    }
+    const label = chars.join('')
+    return {
+      box: hashFn(label),
+      op,
+      focalLength,
+      label
+    }
+  })
+
+  const boxes = []
+  while (boxes.length < 256) {
+    boxes.push({ id: boxes.length, lenses: [], focusPower: 0 })
+  }
+
+  while (instructions.length) {
+    const instruction = instructions.shift()
+    if (instruction.op === '-') {
+      boxes[instruction.box].lenses = boxes[instruction.box].lenses.filter(lens => lens.label !== instruction.label)
+    } else {
+      const existingLens = boxes[instruction.box].lenses.find(lens => lens.label === instruction.label)
+      if (existingLens) {
+        existingLens.focalLength = instruction.focalLength
+      } else {
+        const newLens = {
+          label: instruction.label,
+          focalLength: instruction.focalLength
+        }
+        boxes[instruction.box].lenses.push(newLens)
+      }
+    }
+  }
+
+  const totalFocusPower = boxes.reduce((sum, box) => {
+    box.focusPower = box.lenses.reduce((acc, lens, index) => {
+      console.log('Focusing lens:', lens, 'with power:', (1 + box.id), (1 + index), lens.focalLength)
+      lens.focusPower = (1 + box.id) * (1 + index) * lens.focalLength
+      return acc + lens.focusPower
+    }, 0)
+    box.lenses = box.lenses.map(lens => `[${lens.label} ${lens.focalLength} ${lens.focusPower}]`).join(' ')
+    return sum + box.focusPower
+  }, 0)
+
+  report('Boxes:', boxes.filter(box => box.lenses.length > 0))
+  report('Instructions:', instructions)
+
+  const solution = totalFocusPower
   report('Solution 2:', solution)
 }
 
