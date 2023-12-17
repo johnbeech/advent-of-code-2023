b8c1da2b8e9525ed17fccbd791556ef6620ef9c0
John Beech <johnbeech.github@mkv25.net>
2023-12-17 19:31:38 +0000

    Switch to Dijkstra's algorithm

--- solutions/day17/solution.js
+++ solutions/day17/solution.js
@@ -29,34 +29,24 @@ function parseCityBlocks (input) {
 
   const remapped = blocks.map(row => row.map(block => {
     block.neighbors = neighbors.map(dir => blocks[block.y + dir.y] && blocks[block.y + dir.y][block.x + dir.x]).filter(block => block !== undefined)
+    block.nhl = block.neighbors.reduce((acc, neighbor) => acc + neighbor.val, 0)
     return block
   }))
 
   return remapped
 }
 
-function heuristic (a, b, blocks) {
-  // Euclidean distance as the heuristic
-  const dx = Math.abs(a.x - b.x)
-  const dy = Math.abs(a.y - b.y)
-  const baseHeuristic = Math.sqrt(dx * dx + dy * dy) * 0.01
-  const heatLoss = (blocks[a.x][a.y].val)
-  return baseHeuristic + heatLoss
-}
-
-function findPath (start, end, blocks) {
+function findPath (start, end) {
   const openSet = [start]
   const closedSet = []
   const cameFrom = {}
 
   const gScore = {}
-  const fScore = {}
 
   gScore[start.x + ',' + start.y] = 0
-  fScore[start.x + ',' + start.y] = heuristic(start, end, blocks)
 
   while (openSet.length > 0) {
-    openSet.sort((a, b) => fScore[a.x + ',' + a.y] - fScore[b.x + ',' + b.y])
+    openSet.sort((a, b) => gScore[a.x + ',' + a.y] - gScore[b.x + ',' + b.y])
     let current = openSet.shift()
 
     if (current.x === end.x && current.y === end.y) {
@@ -72,33 +62,42 @@ function findPath (start, end, blocks) {
     closedSet.push(current)
 
     for (const neighbor of current.neighbors) {
-      if (!neighbor || closedSet.some(c => c.x === neighbor.x && c.y === neighbor.y) || neighbor.val > 9) {
+      if (!neighbor || closedSet.some(c => c.x === neighbor.x && c.y === neighbor.y)) {
         continue
       }
 
       const cx = current.x
       const parent = cameFrom[current.x + ',' + current.y]
       const grandParent = parent && cameFrom[parent.x + ',' + parent.y]
-      if (parent?.x === cx && grandParent?.x === cx && neighbor.x === cx) {
-        console.log('Can not move in same X direction a fourth time')
+      const greatGrandParent = grandParent && cameFrom[grandParent.x + ',' + grandParent.y]
+      if (
+        parent?.x === cx &&
+        grandParent?.x === cx &&
+        greatGrandParent?.x === cx &&
+        neighbor.x === cx
+      ) {
+        console.log('Can not move in the same X direction a fourth time')
         continue
       }
 
       const cy = current.y
-      if (parent?.y === cy && grandParent?.y === cy && neighbor.y === cy) {
-        console.log('Can not move in same Y direction a fourth time')
+      if (
+        parent?.y === cy &&
+        grandParent?.y === cy &&
+        greatGrandParent?.y === cy &&
+        neighbor.y === cy
+      ) {
+        console.log('Can not move in the same Y direction a fourth time')
         continue
       }
 
-      const heatLoss = neighbor.val
-      const tentativeGScore = gScore[current.x + ',' + current.y] + heatLoss
+      const tentativeGScore = gScore[current.x + ',' + current.y] + neighbor.val
 
       const neighborKey = neighbor.x + ',' + neighbor.y
 
       if (!openSet.some(o => o.x === neighbor.x && o.y === neighbor.y) || tentativeGScore < gScore[neighborKey]) {
         cameFrom[neighborKey] = current
         gScore[neighborKey] = tentativeGScore
-        fScore[neighborKey] = gScore[neighborKey] + heuristic(neighbor, end, blocks)
 
         if (!openSet.some(o => o.x === neighbor.x && o.y === neighbor.y)) {
           openSet.push(neighbor)
@@ -114,19 +113,19 @@ function findPath (start, end, blocks) {
 function displayCity (city, path) {
   const lines = city.map(row => row.map(block => {
     if (path.some(p => p.x === block.x && p.y === block.y)) {
-      return '#'
+      return block.val
     }
-    return '.'
+    return '.' // block.val
   }).join('')).join('\n')
   console.log(lines)
 }
 
 async function solveForFirstStar (input) {
   const city = parseCityBlocks(input)
-  const start = city[0][0]
+  const start = city[0][1]
   const end = city[city.length - 1][city[0].length - 1]
 
-  const path = findPath(end, start, city)
+  const path = findPath(start, end, city)
   const pathScore = path.reduce((acc, block) => acc + block.val, 0)
 
   const solution = pathScore
