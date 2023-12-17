19aa7a1b258084a075790310a8d1bb79ca11de6b
John Beech <johnbeech.github@mkv25.net>
2023-12-17 20:51:09 +0000

    Correct for example; incorrect for input

--- solutions/day17/solution.js
+++ solutions/day17/solution.js
@@ -11,10 +11,10 @@ async function run () {
 }
 
 const directions = {
-  right: { x: 1, y: 0 },
   down: { x: 0, y: 1 },
-  up: { x: 0, y: -1 },
-  left: { x: -1, y: 0 }
+  right: { x: 1, y: 0 },
+  left: { x: -1, y: 0 },
+  up: { x: 0, y: -1 }
 }
 const neighbors = Object.values(directions)
 
@@ -47,6 +47,7 @@ function findPath (start, end) {
 
   while (openSet.length > 0) {
     openSet.sort((a, b) => gScore[a.x + ',' + a.y] - gScore[b.x + ',' + b.y])
+
     let current = openSet.shift()
 
     if (current.x === end.x && current.y === end.y) {
