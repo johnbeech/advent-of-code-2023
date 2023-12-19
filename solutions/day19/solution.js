2f8f23acf82cd8f74ef2ea176c3ef17f2119070f
John Beech <johnbeech.github@mkv25.net>
2023-12-19 17:30:45 +0000

    Add hard stop for error case

--- solutions/day19/solution.js
+++ solutions/day19/solution.js
@@ -4,7 +4,7 @@ const fromHere = position(__dirname)
 const report = (...messages) => console.log(`[${require(fromHere('../../package.json')).logName} / ${__dirname.split(path.sep).pop()}]`, ...messages)
 
 async function run () {
-  const input = (await read(fromHere('input.txt'), 'utf8')).trim()
+  const input = (await read(fromHere('example.txt'), 'utf8')).trim()
 
   await solveForFirstStar(input)
   await solveForSecondStar(input)
@@ -14,10 +14,16 @@ const acceptedParts = new Set()
 const rejectedParts = new Set()
 
 function acceptPart (part) {
+  if (acceptedParts.has(part)) {
+    throw new Error(`Part ${JSON.stringify(part)} was already accepted`)
+  }
   acceptedParts.add(part)
 }
 
 function rejectPart (part) {
+  if (rejectedParts.has(part)) {
+    throw new Error(`Part ${JSON.stringify(part)} was already rejected`)
+  }
   rejectedParts.add(part)
 }
 
@@ -102,6 +108,12 @@ async function solveForFirstStar (input) {
 
   const tokenFn = {}
 
+  function drainTokens (tokens) {
+    while (tokens.length > 0) {
+      tokens.pop()
+    }
+  }
+
   function startWorkflow (workflowId, part) {
     const workflow = workflows.find(w => w.workflowId === workflowId)
     if (!workflow) {
@@ -113,7 +125,7 @@ async function solveForFirstStar (input) {
     const stack = []
     while (tokens.length > 0) {
       const token = tokens.pop()
-      console.log('P:', part, 'T:', token, 'S:', stack, 'W:', workflowId, 'OTS:', originalTokens.join(' '))
+      console.log('P:', part, 'T:', token, 'TS:', tokens, 'S:', stack, 'W:', workflowId, 'OTS:', originalTokens.join(' '))
       const fn = tokenFn[token]
       if (fn === undefined) {
         stack.push(token)
@@ -127,14 +139,14 @@ async function solveForFirstStar (input) {
     '<': (part, stack, tokens) => {
       const a = stack.pop()
       const b = tokens.pop()
-      console.log('Compare <', a, b, 'stack:', stack, 'tokens:', tokens)
+      console.log('Compare', a, '<', b, 'stack:', stack, 'tokens:', tokens)
       const result = findVal(a, part) < findVal(b, part)
       stack.push(result)
     },
     '>': (part, stack, tokens) => {
       const a = stack.pop()
       const b = tokens.pop()
-      console.log('Compare >', a, b, 'stack:', stack, 'tokens:', tokens)
+      console.log('Compare', a, '>', b, 'stack:', stack, 'tokens:', tokens)
       const result = findVal(a, part) > findVal(b, part)
       stack.push(result)
     },
@@ -150,9 +162,7 @@ async function solveForFirstStar (input) {
         // continue
         tokens.push(b)
       } else if (a === true && b.length > 1) {
-        while (tokens.length > 0) {
-          tokens.pop()
-        }
+        drainTokens(tokens)
         return startWorkflow(b, part)
       } else {
         throw new Error(`Unknown value (a: ${a} ${typeof a}), (b: ${b}) in stack: ${JSON.stringify(stack)}, ${JSON.stringify(tokens)}`)
@@ -164,16 +174,20 @@ async function solveForFirstStar (input) {
         // continue
         tokens.push(b)
       } else if (b.length > 1) {
-        while (tokens.length > 0) {
-          tokens.pop()
-        }
+        drainTokens(tokens)
         return startWorkflow(b, part)
       } else {
         throw new Error(`Unknown value (b: ${b} ${typeof b}) in stack: ${JSON.stringify(stack)}, ${JSON.stringify(tokens)}`)
       }
     },
-    A: (part) => acceptPart(part),
-    R: (part) => rejectPart(part)
+    A: (part, tokens) => {
+      drainTokens(tokens)
+      acceptPart(part)
+    },
+    R: (part, tokens) => {
+      drainTokens(tokens)
+      rejectPart(part)
+    }
   })
 
   parts.forEach(part => {
