dcf87906b4d88de323c10653a1257316c96dec75
John Beech <johnbeech.github@mkv25.net>
2023-12-21 16:42:29 +0000

    Parse garden input for Day 21

---
+++ solutions/day21/solution.js
@@ -0,0 +1,61 @@
+const path = require('path')
+const { read, position } = require('promise-path')
+const fromHere = position(__dirname)
+const report = (...messages) => console.log(`[${require(fromHere('../../package.json')).logName} / ${__dirname.split(path.sep).pop()}]`, ...messages)
+
+async function run () {
+  const input = (await read(fromHere('example.txt'), 'utf8')).trim()
+
+  await solveForFirstStar(input)
+  await solveForSecondStar(input)
+}
+
+const directions = {
+  N: { x: 0, y: -1 },
+  E: { x: 1, y: 0 },
+  S: { x: 0, y: 1 },
+  W: { x: -1, y: 0 }
+}
+
+const neighbors = Object.values(directions)
+
+function parseGardenMap (input) {
+  const gardenMap = input.split('\n').map((line, y) => {
+    return line.split('').map((char, x) => {
+      return {
+        char, x, y
+      }
+    })
+  })
+  const gardenTiles = gardenMap.flat(2).map(tile => {
+    tile.neighbors = neighbors.map(direction => {
+      const neighbor = gardenMap[tile.y + direction.y]?.[tile.x + direction.x]
+      return neighbor
+    }).filter(neighbor => neighbor)
+    return tile
+  })
+  const startTile = gardenTiles.find(tile => tile.char === 'S')
+  return {
+    gardenMap,
+    gardenTiles,
+    width: gardenMap[0].length,
+    height: gardenMap.length,
+    startTile
+  }
+}
+
+async function solveForFirstStar (input) {
+  const { gardenMap, gardenTiles, width, height, startTile } = parseGardenMap(input)
+
+  console.log({ gardenMap, gardenTiles, width, height, startTile })
+
+  const solution = 'UNSOLVED'
+  report('Solution 1:', solution)
+}
+
+async function solveForSecondStar (input) {
+  const solution = 'UNSOLVED'
+  report('Solution 2:', solution)
+}
+
+run()
