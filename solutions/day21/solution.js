a7eb72b4a21db6afca86d3203e0fa7f87e6e7c02
John Beech <johnbeech.github@mkv25.net>
2023-12-21 22:51:39 +0000

    Solve day 21 for first star

--- solutions/day21/solution.js
+++ solutions/day21/solution.js
@@ -4,17 +4,17 @@ const fromHere = position(__dirname)
 const report = (...messages) => console.log(`[${require(fromHere('../../package.json')).logName} / ${__dirname.split(path.sep).pop()}]`, ...messages)
 
 async function run () {
-  const input = (await read(fromHere('example.txt'), 'utf8')).trim()
+  const input = (await read(fromHere('input.txt'), 'utf8')).trim()
 
   await solveForFirstStar(input)
   await solveForSecondStar(input)
 }
 
 const directions = {
-  N: { x: 0, y: -1 },
-  E: { x: 1, y: 0 },
-  S: { x: 0, y: 1 },
-  W: { x: -1, y: 0 }
+  N: { x: 0, y: -1, d: 'N' },
+  E: { x: 1, y: 0, d: 'E' },
+  S: { x: 0, y: 1, d: 'S' },
+  W: { x: -1, y: 0, d: 'W' }
 }
 
 const neighbors = Object.values(directions)
@@ -23,17 +23,14 @@ function parseGardenMap (input) {
   const gardenMap = input.split('\n').map((line, y) => {
     return line.split('').map((char, x) => {
       return {
-        char, x, y
+        char,
+        x,
+        y,
+        key: `${x},${y}`
       }
     })
   })
-  const gardenTiles = gardenMap.flat(2).map(tile => {
-    tile.neighbors = neighbors.map(direction => {
-      const neighbor = gardenMap[tile.y + direction.y]?.[tile.x + direction.x]
-      return neighbor
-    }).filter(neighbor => neighbor)
-    return tile
-  })
+  const gardenTiles = gardenMap.flat(2)
   const startTile = gardenTiles.find(tile => tile.char === 'S')
   return {
     gardenMap,
@@ -44,18 +41,33 @@ function parseGardenMap (input) {
   }
 }
 
-function displayGardenMap (gardenMap) {
-  return gardenMap.map(line => line.map(tile => tile.char).join('')).join('\n')
-}
-
 async function solveForFirstStar (input) {
-  const { gardenMap, gardenTiles, width, height, startTile } = parseGardenMap(input)
+  const { gardenMap, startTile } = parseGardenMap(input)
 
-  console.log({ gardenMap, gardenTiles, width, height, startTile })
+  let visited = new Set()
+  let endTiles = [startTile]
 
-  console.log(displayGardenMap(gardenMap))
+  let counter = 0
+  while (counter < 64) {
+    const toExplore = endTiles
+    endTiles = []
+    visited = new Set()
+    for (const { x, y } of toExplore) {
+      for (const direction of neighbors) {
+        const newx = x + direction.x
+        const newy = y + direction.y
+        const tile = gardenMap[newy][newx]
+        if (tile.char !== '#' && !visited.has(tile)) {
+          visited.add(tile)
+          endTiles.push(tile)
+        }
+      }
+    }
 
-  const solution = 'UNSOLVED'
+    counter++
+  }
+
+  const solution = endTiles.length
   report('Solution 1:', solution)
 }
 
