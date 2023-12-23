9e52232ad24d4f7dfb7d9d9be7c7880aca664f1d
John Beech <johnbeech.github@mkv25.net>
2023-12-23 20:18:59 +0000

    Solve day 23 for second star by adapting surg1's solution

--- solutions/day23/solution.js
+++ solutions/day23/solution.js
@@ -42,7 +42,7 @@ function parseForestMap (input) {
           } else if (tile.char === '.') {
             return true
           } else if (slopeChars.includes(tile.char)) {
-            console.log('Found slope neighbor:', tile, 'from', cell)
+            // console.log('Found slope neighbor:', tile, 'from', cell)
             const slope = neighbors.find(({ slope }) => slope === tile.char)
             return slope === coord.dir
           } else {
@@ -69,6 +69,118 @@ function parseForestMap (input) {
   }
 }
 
+function findAllPaths (forestMap, start, end) {
+  const longestPath = []
+  const open = []
+  // Walk all possible paths; entering each tile only once
+  open.push({ path: [start], tile: start })
+  let steps = 0
+  while (open.length > 0) {
+    const { path, tile } = open.shift()
+    if (tile === end) {
+      if (path.length > longestPath.length) {
+        longestPath.length = 0
+        longestPath.push(...path)
+      }
+    }
+    tile.neighbors.forEach(coord => {
+      const neighbor = forestMap[coord.y]?.[coord.x]
+      if (!path.includes(neighbor)) {
+        open.push({ path: [...path, neighbor], tile: neighbor })
+      }
+    })
+    if (steps % 10000 === 0) {
+      console.log('Find step', steps, 'Open', open.length, 'Longest path', longestPath.length)
+    }
+    steps++
+  }
+
+  return longestPath
+}
+
+function connectToJunction (node, nodes) {
+  // try to locate existing one
+  let jncId = nodes.findIndex(n => n.p[0] === node.p[0] && n.p[1] === node.p[1])
+
+  if (jncId === node.lastJncId) {
+    return jncId
+  }
+
+  if (jncId === -1) {
+    jncId = nodes.push({ p: node.p.slice(), connections: [] }) - 1
+  }
+
+  // we need to connect cur.lastJuncId and newJuncId
+  if (nodes[node.lastJncId].connections.findIndex(conn => conn.id === jncId) === -1) {
+    nodes[node.lastJncId].connections.push({
+      id: jncId,
+      distance: node.steps - node.stepsToLastJnc
+    })
+  }
+
+  if (nodes[jncId].connections.findIndex(conn => conn.id === node.lastJncId) === -1) {
+    nodes[jncId].connections.push({
+      id: node.lastJncId,
+      distance: node.steps - node.stepsToLastJnc
+    })
+  }
+
+  return jncId
+}
+
+function key (p) {
+  return p[0] + '_' + p[1]
+}
+
+function addVect (a, b) {
+  return a.map((v, c) => v + b[c])
+}
+
+function validPos (p, forestMap) {
+  return forestMap[p[1]] !== undefined && forestMap[p[1]][p[0]] !== undefined && forestMap[p[1]][p[0]] !== '#'
+}
+
+function getGraph (startPos, endPos, forestMap) {
+  const open = [{ p: startPos.slice(), steps: 0, lastJncId: 0, stepsToLastJnc: 0 }]
+  const nodes = [{ p: [1, 0], connections: [] }]
+  const visited = {}
+  let steps = 0
+
+  while (open.length) {
+    steps++
+    if (steps % 1000 === 0) {
+      console.log('Graph step', steps, 'Open', open.length, 'Nodes', nodes.length)
+    }
+    const node = open.pop()
+    const k = key(node.p)
+    const moves = neighbors.map(dir => addVect(node.p, [dir.x, dir.y])).filter(pos => validPos(pos, forestMap))
+
+    if (moves.length > 2) {
+      node.lastJncId = connectToJunction(node, nodes)
+      node.stepsToLastJnc = node.steps
+    }
+
+    if (visited[k] !== undefined) {
+      continue
+    }
+    visited[k] = 1
+
+    if (node.p[0] === endPos[0] && node.p[1] === endPos[1]) {
+      connectToJunction(node, nodes)
+      continue
+    }
+
+    moves.forEach(np => open.push({
+      p: np,
+      steps: node.steps + 1,
+      lastJncId: node.lastJncId,
+      stepsToLastJnc: node.stepsToLastJnc
+    }))
+  }
+
+  return nodes
+}
+
 async function solveForFirstStar (input) {
   const { forestMap, entryTile, exitTile } = parseForestMap(input)
 
@@ -77,32 +189,8 @@ async function solveForFirstStar (input) {
 
   await write(fromHere('forest-map.json'), JSON.stringify(forestMap, null, 2), 'utf8')
 
-  const paths = []
-  const open = []
-  // Walk all possible paths; entering each tile only once
-  open.push({ path: [entryTile], tile: entryTile })
-  let steps = 0
-  while (open.length > 0) {
-    const { path, tile } = open.shift()
-    if (tile === exitTile) {
-      paths.push(path)
-    }
-    tile.neighbors.forEach(coord => {
-      const neighbor = forestMap[coord.y]?.[coord.x]
-      if (!path.includes(neighbor)) {
-        open.push({ path: [...path, neighbor], tile: neighbor })
-      }
-    })
-    if (steps % 1000 === 0) {
-      console.log('Step', steps, 'Open', open.length, 'Paths', paths.length)
-    }
-    steps++
-  }
+  const longestPath = findAllPaths(forestMap, entryTile, exitTile)
 
-  paths.sort((a, b) => b.length - a.length)
-  console.log('Paths', paths.length, paths.map(path => path.length - 1), 'Steps', steps)
-
-  const longestPath = paths[0]
   const longestDisplayMap = displayMap(forestMap, longestPath)
   await write(fromHere('solution1-map.txt'), longestDisplayMap, 'utf8')
 
@@ -123,7 +211,41 @@ function displayMap (forestMap, path) {
 }
 
 async function solveForSecondStar (input) {
-  const solution = 'UNSOLVED'
+  // Adapted from https://github.com/surgi1/adventofcode/blob/main/2023/day23/script.js
+  const forestMap = input.split('\n').map((line, y) => line.split(''))
+  const startPos = [1, 0]
+  const endPos = [forestMap[0].length - 2, forestMap.length - 1]
+
+  const nodes = getGraph(startPos, endPos, forestMap)
+
+  const stack = [{ p: 0, steps: 0, seen: {} }]
+  const endNodeId = nodes.length - 1
+  let maxSteps = 0
+
+  let steps = 0
+  while (stack.length) {
+    steps++
+    if (steps % 100000 === 0) {
+      console.log('Search step', steps, 'Stack', stack.length)
+    }
+    const cur = stack.pop()
+
+    const k = cur.p
+    cur.seen[k] = 1
+
+    if (cur.p === endNodeId) {
+      maxSteps = Math.max(cur.steps, maxSteps)
+      continue
+    }
+
+    nodes[k].connections.filter(n => cur.seen[n.id] === undefined).forEach(n => stack.push({
+      p: n.id,
+      steps: cur.steps + n.distance,
+      seen: { ...cur.seen }
+    }))
+  }
+
+  const solution = maxSteps
   report('Solution 2:', solution)
 }
 
